e1badd70e7c75b0cb1a25de160002d004f211091
Ivan Burgos <ivan.pburgos@gmail.com>
2022-01-16 20:05:06 -0600

    edit webpack config to accept images and implement multiple loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const webpack = require("webpack");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
@@ -22,17 +23,36 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
+      cache: false,
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   resolve: {
     alias: {
-      Helpers: path.resolve(__dirname, "src/helpers/"),
       GeneralComponents: path.resolve(__dirname, "src/general_components/"),
+      Helpers: path.resolve(__dirname, "src/helpers/"),
+      Images: path.resolve(__dirname, "src/images/"),
       Views: path.resolve(__dirname, "src/views/"),
     },
   },
